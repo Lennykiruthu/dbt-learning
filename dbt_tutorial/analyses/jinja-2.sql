4fe3711add1e7bf600734bce8bd9a8e2208534bf
Lenny Kiruthu <kiruthulenny@gmail.com>
2026-01-30 17:07:09 +0300

    Finished tutorial

--- dbt_tutorial/analyses/jinja-2.sql
+++ dbt_tutorial/analyses/jinja-2.sql
@@ -1,5 +1,11 @@
 {%- set apples = ["Gala", "Red Delicious", "Fuji", "McIntosh", "Honeycrisp"] -%}
 
 {% for i in apples %}
-    {{ i }}
+    
+    {% if i != "McIntosh" %}
+        {{ i }}
+    {% else %}
+        I hate {{ i }}
+    {% endif %}
+    
 {% endfor %}
---
+++ dbt_tutorial/analyses/query-macro.sql
@@ -0,0 +1,3 @@
+SELECT
+    {{ multiply(10, 50) }} as tes_col
+    
